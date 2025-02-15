de575f499a07bfcdb755ba1524ed35a8e69b47bf
GUERMEUR-Nicolas <ng.stcharles@gmail.com>
2025-02-15 17:40:37 -0500

    Step 6

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -37,6 +37,9 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -78,7 +81,9 @@ fun TipTimeLayout() {
 				.padding(bottom = 16.dp, top = 40.dp)
 				.align(alignment = Alignment.Start)
 		)
-		EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+		EditNumberField(modifier = Modifier
+			.padding(bottom = 32.dp)
+			.fillMaxWidth())
 		Text(
 			text = stringResource(R.string.tip_amount, "$0.00"),
 			style = MaterialTheme.typography.displaySmall
@@ -99,10 +104,10 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-	var amountInput: MutableState<String> = mutableStateOf("0")
+	var amountInput by remember { mutableStateOf("") }
 	TextField(
-		value = amountInput.value,
-		onValueChange = { amountInput.value = it },
+		value = amountInput,
+		onValueChange = { amountInput = it },
 		modifier = modifier
 	)
 }
