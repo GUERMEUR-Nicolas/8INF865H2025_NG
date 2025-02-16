405e461115fa3ab3b0fdf6d8cf0ef95441167bce
GUERMEUR-Nicolas <ng.stcharles@gmail.com>
2025-02-16 14:44:22 -0500

    Step 6

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -22,6 +22,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -29,6 +31,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -36,6 +39,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -52,6 +56,7 @@ import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
 import com.example.tiptime.ui.theme.md_theme_dark_onSurfaceVariant
 import java.text.NumberFormat
+import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -73,9 +78,10 @@ class MainActivity : ComponentActivity() {
 fun TipTimeLayout() {
 	var amountInput by remember { mutableStateOf("") }
 	var tipInput by remember { mutableStateOf("")}
+	var roundUp by remember { mutableStateOf(false) }
 	val amount = amountInput.toDoubleOrNull() ?: 0.0
 	val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
-	val tip = calculateTip(amount, tipPercent)
+	val tip = calculateTip(amount, tipPercent, roundUp)
 	Column(
 		modifier = Modifier
 			.statusBarsPadding()
@@ -91,6 +97,7 @@ fun TipTimeLayout() {
 				.padding(bottom = 16.dp, top = 40.dp)
 				.align(alignment = Alignment.Start)
 		)
+		Spacer(modifier = Modifier.height(150.dp))
 		EditNumberField(
 			label = R.string.bill_amount,
 			keyboardOptions = KeyboardOptions.Default.copy(
@@ -115,6 +122,11 @@ fun TipTimeLayout() {
 				.padding(bottom = 32.dp)
 				.fillMaxWidth()
 		)
+		RoundTheTipRow(
+			roundUp = roundUp,
+			onRoundUpChanged = { roundUp = it },
+			modifier = Modifier.padding(bottom = 32.dp)
+		)
 		Text(
 			text = stringResource(R.string.tip_amount, tip),
 			style = MaterialTheme.typography.displaySmall
@@ -128,8 +140,15 @@ fun TipTimeLayout() {
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-	val tip = tipPercent / 100 * amount
+private fun calculateTip(
+	amount: Double,
+	tipPercent: Double = 15.0,
+	roundUp: Boolean
+): String {
+	var tip = tipPercent / 100 * amount
+	if (roundUp) {
+		tip = kotlin.math.ceil(tip)
+	}
 	return NumberFormat.getCurrencyInstance().format(tip)
 }
 
@@ -149,6 +168,29 @@ fun EditNumberField(
 		keyboardOptions = keyboardOptions,
 		modifier = modifier
 	)
+}
+
+@Composable
+fun RoundTheTipRow(
+	roundUp: Boolean,
+	onRoundUpChanged: (Boolean) -> Unit,
+	modifier: Modifier = Modifier
+) {
+	Row(
+		modifier = modifier
+			.fillMaxWidth()
+			.size(48.dp),
+		verticalAlignment = Alignment.CenterVertically
+	) {
+		Text(text = stringResource(R.string.round_up_tip))
+		Switch(
+			checked = roundUp,
+			onCheckedChange = onRoundUpChanged,
+			modifier = modifier
+				.fillMaxWidth()
+				.wrapContentWidth(Alignment.End)
+		)
+	}
 }
 
 @Preview(showBackground = true)
