b5b9804d844e13f36026e89a976f54732516ddd3
GUERMEUR-Nicolas <ng.stcharles@gmail.com>
2025-02-16 14:28:39 -0500

    Step 5

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -45,6 +45,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -92,6 +93,10 @@ fun TipTimeLayout() {
 		)
 		EditNumberField(
 			label = R.string.bill_amount,
+			keyboardOptions = KeyboardOptions.Default.copy(
+				keyboardType = KeyboardType.Number,
+				imeAction = ImeAction.Next
+			),
 			value = amountInput,
 			onValueChange = { amountInput = it },
 			modifier = Modifier
@@ -100,6 +105,10 @@ fun TipTimeLayout() {
 		)
 		EditNumberField(
 			label = R.string.how_was_the_service,
+			keyboardOptions = KeyboardOptions.Default.copy(
+				keyboardType = KeyboardType.Number,
+				imeAction = ImeAction.Done
+			),
 			value = tipInput,
 			onValueChange = { tipInput = it },
 			modifier = Modifier
@@ -127,6 +136,7 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 @Composable
 fun EditNumberField(
 	@StringRes label: Int,
+	keyboardOptions: KeyboardOptions,
 	value: String,
 	onValueChange: (String) -> Unit,
 	modifier: Modifier = Modifier
@@ -136,7 +146,7 @@ fun EditNumberField(
 		onValueChange = onValueChange,
 		label = { Text(stringResource(label))},
 		singleLine = true,
-		keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+		keyboardOptions = keyboardOptions,
 		modifier = modifier
 	)
 }
