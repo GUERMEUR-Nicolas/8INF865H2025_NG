56f85069b98273c33b08c63b9b83a82555e894c4
GUERMEUR-Nicolas <ng.stcharles@gmail.com>
2025-02-16 14:22:30 -0500

    Step 4

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -70,8 +71,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeLayout() {
 	var amountInput by remember { mutableStateOf("") }
+	var tipInput by remember { mutableStateOf("")}
 	val amount = amountInput.toDoubleOrNull() ?: 0.0
-	val tip = calculateTip(amount)
+	val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+	val tip = calculateTip(amount, tipPercent)
 	Column(
 		modifier = Modifier
 			.statusBarsPadding()
@@ -88,11 +91,21 @@ fun TipTimeLayout() {
 				.align(alignment = Alignment.Start)
 		)
 		EditNumberField(
+			label = R.string.bill_amount,
 			value = amountInput,
 			onValueChange = { amountInput = it },
 			modifier = Modifier
 				.padding(bottom = 32.dp)
-				.fillMaxWidth())
+				.fillMaxWidth()
+		)
+		EditNumberField(
+			label = R.string.how_was_the_service,
+			value = tipInput,
+			onValueChange = { tipInput = it },
+			modifier = Modifier
+				.padding(bottom = 32.dp)
+				.fillMaxWidth()
+		)
 		Text(
 			text = stringResource(R.string.tip_amount, tip),
 			style = MaterialTheme.typography.displaySmall
@@ -113,6 +126,7 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 
 @Composable
 fun EditNumberField(
+	@StringRes label: Int,
 	value: String,
 	onValueChange: (String) -> Unit,
 	modifier: Modifier = Modifier
@@ -120,7 +134,7 @@ fun EditNumberField(
 	TextField(
 		value = value,
 		onValueChange = onValueChange,
-		label = { Text(stringResource(R.string.bill_amount))},
+		label = { Text(stringResource(label))},
 		singleLine = true,
 		keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
 		modifier = modifier
