e58571495fe298089b26affc6270ae18f2ee1029
GUERMEUR-Nicolas <ng.stcharles@gmail.com>
2025-02-16 19:09:20 -0500

    Step 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.navigation:navigation-compose:2.7.4")
+	implementation("androidx.navigation:navigation-runtime-ktx:2.8.7")
 
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+	debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
