2691f53c4651a8c048f51b9c3d56063a131aca66
GUERMEUR-Nicolas <ng.stcharles@gmail.com>
2025-02-18 17:01:25 -0500

    Step 8

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -8,6 +8,7 @@ import com.example.unscramble.data.allWords
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import com.example.unscramble.data.SCORE_INCREASE
 import kotlinx.coroutines.flow.update
 
 
@@ -50,11 +51,29 @@ class GameViewModel : ViewModel() {
 
 	fun checkUserGuess() {
 		if (userGuess.equals(currentWord, ignoreCase = true)) {
+			val updatedScore = _uiState.value.score.plus(SCORE_INCREASE)
+			updateGameState(updatedScore)
 		} else {
 			_uiState.update { currentState ->
 				currentState.copy(isGuessedWordWrong = true)
 			}
 		}
+		updateUserGuess("")
+	}
+
+	private fun updateGameState(updatedScore: Int) {
+		_uiState.update { currentState ->
+			currentState.copy(
+				isGuessedWordWrong = false,
+				currentScrambledWord = pickRandomWordAndShuffle(),
+				score = updatedScore,
+				currentWordCount = currentState.currentWordCount.inc()
+			)
+		}
+	}
+
+	fun skipWord() {
+		updateGameState(_uiState.value.score)
 		updateUserGuess("")
 	}
 
@@ -65,5 +84,7 @@ class GameViewModel : ViewModel() {
 
 data class GameUiState(
 	val currentScrambledWord: String = "",
+	val currentWordCount: Int = 1,
 	val isGuessedWordWrong: Boolean = false,
+	val score: Int = 0
 )
